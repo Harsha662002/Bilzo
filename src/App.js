18261336f3cbb55564c14cd2aea9a8db744b8a45
Harsha662002 <hvasireddy329@gmail.com>
2024-04-18 19:02:39 +0530

    Carousel Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import Carousel from "./components/carousel/carousel";
 import Hero from "./components/hero/hero";
 import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
+      <Navbar />
       <Hero />
+      <Carousel />
     </div>
   );
 }
