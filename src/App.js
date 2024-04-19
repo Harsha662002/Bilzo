3c03c31f0e4c5d879b59d40c7a27e9fc3601b002
Harsha662002 <hvasireddy329@gmail.com>
2024-04-19 13:55:51 +0530

    Footer Added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import Carousel from "./components/carousel/carousel";
 import Customers from "./components/customers/customers";
 import Features from "./components/features/features";
+import Footer from "./components/footer/footer";
 import Hero from "./components/hero/hero";
 import Integrations from "./components/integrations/integrations";
 import Navbar from "./components/navbar/navbar";
+import Trial from "./components/trial/trial";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar /> */}
       <Hero />
       <Carousel />
       <Features />
       <Integrations />
       <Customers />
+      <Trial />
+      <Footer />
     </div>
   );
 }
